d558c64e59fd12e94ecc513e0772e7e86370022f
levi-brandon <ltrejo32@gmail.com>
2017-08-02 15:37:15 -0500

    Initial round of bash scipts for Linux Academy scripting course

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bash_scripting
+
+test.sh - A very quick bash script example
+
+env.sh - A script that uses environment variables heavily as an example for later, more complex scripts
---
+++ env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+
+echo "This script will give us environment information"
+echo "================================================"
+echo ""
+echo "Hello Username: $USER "
+echo ""
+echo "Your home directory is: $HOME"
+echo ""
+echo "Your History File will ignore: $HISTCONTROL"
+echo ""
+echo "Your terminal session type is: $TERM"
+echo ""
