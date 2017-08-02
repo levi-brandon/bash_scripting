83887434aff7a2dc209780335d13c78e53eaa249
levi-brandon <ltrejo32@gmail.com>
2017-08-02 16:42:49 -0500

    Added comments.sh and updated README

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ test.sh - A very quick bash script example
 env.sh - A script that uses environment variables heavily as an example for later, more complex scripts
 
 varexample.sh - Another variable example script
+
+comments.sh - A script to show the usage of comments
---
+++ comments.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This line is intended to be used as a general description of the script
+# and anything it does
+#
+
+clear    # clears the screen
+
+# MYUSERNAME="Levi"    # the username for this application
+
+MYUSERNAME="Don"  # Newly created user name
+
+echo "We are using the default user called: $MYUSERNAME" # display to the console
+
+DATETIMESTAMP=`date`
+
+echo "This is when the script was run: $DATETIMESTAMP" # this is the timestamp of run
