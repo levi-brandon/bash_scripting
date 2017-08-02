1d96c2419f701f6d7ef4bc519caad1b96c4ad994
levi-brandon <ltrejo32@gmail.com>
2017-08-02 16:11:56 -0500

    Updated varexample.sh to a more useful example

--- varexample.sh
+++ varexample.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "The current path is: $PATH"
+MYUSERNAME="username"
+MYPASSWORD="password123"
+STARTOFSCRIPT=`date`
+
+echo "My login name for this application is: $MYUSERNAME"
+echo "My login password for this application is: $MYPASSWORD"
+echo "I started this script at: $STARTOFSCRIPT"
+
+sleep 2
+
+ENDOFSCRIPT=`date`
+
+echo "I ended this script at: $ENDOFSCRIPT"
